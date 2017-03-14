e3a92a4a7b04b4dcf54e7304007c62fec3667f8a
Briaker <mail@brianbaker.ca>
2017-03-14 12:09:02 -0400

    removed extra component

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Searchbar from './searchbar';
-
-console.log('react app loaded!');
 
 class App extends React.Component {
-    displayName(name) {
-        return (<p>{name}</p>);
-    }
-
     render() {
         return (
             <div>
                 <h1>React App Loaded!</h1>
                 <p>Good work!</p>
-                {this.displayName('Brian')}
-                <Searchbar />
             </div>
         );
     }
