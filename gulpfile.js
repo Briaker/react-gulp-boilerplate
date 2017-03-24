90f0fdbcb3fa992a089bdd76dc0c03d2ab9cb6bd
Briaker <mail@brianbaker.ca>
2017-03-24 16:21:31 -0400

    Added image and css minifiers and source maps for js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 const gulp          = require('gulp');
+const imagemin      = require('gulp-imagemin');
 const sass          = require('gulp-sass');
 const concat        = require('gulp-concat');
 const autoprefixer  = require('gulp-autoprefixer');
 const notify        = require('gulp-notify');
 const plumber       = require('gulp-plumber');
-const browserSync   = require('browser-sync').create();
 
 const babelify      = require('babelify');
+const stageTwo      = require('babel-preset-stage-2');
 const browserify    = require('browserify');
 const buffer        = require('vinyl-buffer');
 const source        = require('vinyl-source-stream');
+const uglify        = require('gulp-uglify');
 
+const browserSync   = require('browser-sync').create();
 const reload        = browserSync.reload;
 
 gulp.task('browser-sync', () => {
@@ -25,20 +28,31 @@ gulp.task('html', () => {
     .pipe(reload({stream: true}));
 });
 
+gulp.task('images', () => {
+    return gulp.src('./src/assets/images/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/assets/images/'))
+    .pipe(reload({stream: true}));
+});
+
 gulp.task('styles', () => {
     return gulp.src('./src/assets/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./dist/assets/styles/'))
     .pipe(reload({stream: true}));
 });
 
-gulp.task('es6', () => {
+gulp.task('es6-dev', () => {
     return browserify('./src/app.js', { debug: true })
     .transform('babelify', {
         sourceMaps: true,
-        presets: ['es2015', 'react']
+        presets: [
+            'es2015', 
+            'react', 
+            'stage-2'
+        ]
     })
     .bundle()
     .on('error',notify.onError({
@@ -47,16 +61,38 @@ gulp.task('es6', () => {
     }))
     .pipe(source('app.js'))
     .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/assets/js'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('es6', () => {
+    return browserify('./src/app.js')
+    .transform('babelify', {
+        presets: [
+            'es2015', 
+            'react', 
+            'stage-2'
+        ]
+    })
+    .bundle()
+    .on('error',notify.onError({
+        message: "Error: <%= error.message %>",
+        title: 'Error in JS 💀'
+    }))
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./dist/assets/js'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('watch', () => {
     gulp.watch('./src/assets/styles/**/*.scss', ['styles']);
-    gulp.watch('./src/**/*.js', ['es6']);
+    gulp.watch('./src/**/*.js', ['es6-dev']);
     gulp.watch('./src/*.html', ['html']);
 });
 
-gulp.task('build', ['html', 'styles', 'es6']);
+gulp.task('build', ['html', 'images', 'styles', 'es6']);
 
-gulp.task('default', ['html', 'styles', 'es6', 'browser-sync', 'watch']);
+gulp.task('default', ['html', 'images', 'styles', 'es6-dev', 'browser-sync', 'watch']);
